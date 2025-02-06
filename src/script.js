b42549218171cf5f33b2b6e265090d9fd030ce04
luismottaa <luispires1222@gmail.com>
2025-02-06 10:26:00 -0400

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    const img = document.querySelector("#profile img")
+
+
+
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './imagens/gojo.jpg')
+    } else {
+        img.setAttribute("src", "./imagens/black gojo.png")
+
+    }
+
+}
+
